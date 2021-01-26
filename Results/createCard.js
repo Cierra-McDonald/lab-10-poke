fc887a2509318490cb259c3f249a291be0e20e5a
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-26 01:54:46 -0700

    TODO result page

--- Results/createCard.js
+++ Results/createCard.js
@@ -1,31 +1,31 @@
-import {  } from "module";
+// import { getPokeStats } from '../localStorageUtils.js';
 
 
-export function renderPokeResults(pokemon, pokemonStat) {
-    const outerDiv = document.createElement('div');
-    const img = document.createElement('img');
-    const innerDiv = document.createElement('div');
-    const pSeen = document.createElement('p');
-    const pCaught = document.createElement('p');
+// export function renderPokeResults(getPokeStats) {
+//     const outerDiv = document.createElement('div');
+//     const img = document.createElement('img');
+//     const innerDiv = document.createElement('div');
+//     const pSeen = document.createElement('p');
+//     const pCaught = document.createElement('p');
 
-    outerDiv.classList.add('all-results');
+//     outerDiv.classList.add('all-results');
 
 
-    img.classList.add('pokemon-img');
-    img.src = pokemon.url_image;
+//     img.classList.add('pokemon-img');
+//     img.src = pokemon.url_image;
 
-    innerDiv.classList.add('card-body');
+//     innerDiv.classList.add('card-body');
 
-    pSeen.classList.add('all-seen');
-    pSeen.textContent = `Pokemon seen: ${}`;
+//     pSeen.classList.add('all-seen');
+//     pSeen.textContent = `Pokemon seen: ${pokemonStat.seen}`;
 
-    pCaught.classList.add('all-caught');
-    pCaught.textContent = `Pokemon caught: ${}`;
+//     pCaught.classList.add('all-caught');
+//     pCaught.textContent = `Pokemon caught: ${pokemonStat.caught}`;
 
-    outerDiv.append(img);
-    outerDiv.append(innerDiv);
-    innerDiv.append(pSeen);
-    innerDiv.append(pCaught);
+//     outerDiv.append(img);
+//     outerDiv.append(innerDiv);
+//     innerDiv.append(pSeen);
+//     innerDiv.append(pCaught);
 
-}
+// }
 
